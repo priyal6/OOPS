b8230bf55924a4bbd177d295d6f1b4125c570ec9
priyal6 <layirps06@gmail.com>
2025-10-07 10:23:46 +0100

    adv

--- adv inheritance.py
+++ adv inheritance.py
@@ -47,25 +47,32 @@
 
 #multilevel
 class A:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-    def greet(self):
-        print(f"Hello from A, {self.name}")
 
-class B(A):
     def greet(self):
-        print(f"Hello from A, {self.name}")
+        print(f"Hello from A, {self.name}.")
+
+# Intermediate class 1
+class B(A):
+
+    def greet(self):
+        print(f"Hello from B, {self.name}.")
         super().greet()
-        
+
+# Intermediate class 2
 class C(A):
     def greet(self):
-        print(f"Hello from C, {self.name}")
+        print(f"Hello from C, {self.name}.")
         super().greet()
 
-class D(B,C):
+# Derived class
+class D(B, C):
+
     def greet(self):
-        print(f"Hello from D, {self.name}")
+        print(f"Hello from D, {self.name}.")
         super().greet()
 
+# Create an instance of D
 d = D("Frank")
 d.greet()
